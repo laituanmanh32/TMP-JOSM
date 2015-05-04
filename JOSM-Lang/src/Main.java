93243c312519ab688cae99797504b7db89faea18
Lại Tuấn Mạnh <laituanmanh32@gmail.com>
2015-05-04 09:10:28 +0700

    Merge method in POFileHandler

--- JOSM-Lang/src/Main.java
+++ JOSM-Lang/src/Main.java
@@ -1,30 +1,11 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		POFileHandler pohdl  = new POFileHandler();
-		List<String> a = pohdl.getMsgid(new File("josm_josm-vi.po"));
-
-		writeListToFile(a, "msgid.po");
-	}
-
-	public static void writeListToFile(List lst, String filename) throws IOException{
-		File outFile = new File(filename);
-		outFile.createNewFile();
-
-		PrintWriter pw = new PrintWriter(outFile);
-
-		for(int i = 0 ; i < lst.size(); i++){
-			pw.println(lst.get(i));
-			pw.flush();
-		}
-
+		POFileHandler.repairMsgstrFile(new File("msgid.po"), new File("msgstr.po"));
+		POFileHandler.mergePOFile(new File("josm_josm-vi.po"), new File("repaiedmsgstr.po"), new File("vi.po"));
 	}
 
 }
--- JOSM-Lang/src/POFileHandler.java
+++ JOSM-Lang/src/POFileHandler.java
@@ -3,11 +3,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-public class POFileHandler {
+public final class POFileHandler {
 	/**
 	 * Fetch all line belong to msgid line.
 	 *
@@ -15,23 +17,21 @@ public class POFileHandler {
 	 * @return list of msgid line.
 	 * @throws IOException
 	 */
-	public List<String> getMsgid(File poFile) throws IOException {
+	public static LinkedList<List<String>> getMsgid(File poFile)
+			throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(poFile));
-		List<List<String>> msgidList = new LinkedList<List<String>>();
+		LinkedList<List<String>> msgidList = new LinkedList<List<String>>();
 		List<String> msgid = new ArrayList<String>();
 
 		String newline = br.readLine();
-		while (true) {
-			if (!br.ready())
-				break;
+		while (br.ready()) {
 			if (newline.startsWith("msgid")) {
 				msgid = new ArrayList<String>();
 				msgid.add(newline);
 				for (;;) {
 					newline = br.readLine();
 					if (newline.startsWith("msgstr")
-							|| newline.startsWith("msgid")
-							|| newline == null)
+							|| newline.startsWith("msgid") || newline == null)
 						break;
 					msgid.add(newline);
 				}
@@ -39,11 +39,13 @@ public class POFileHandler {
 			} else {
 				newline = br.readLine();
 			}
+			System.out.println(newline);
+			System.out.flush();
 		}
 
 		System.out.println(msgidList.get(1));
 		br.close();
-		return msgid;
+		return msgidList;
 	}
 
 	/**
@@ -53,33 +55,123 @@ public class POFileHandler {
 	 * @return
 	 * @throws IOException
 	 */
-	public List<List<String>> getMsgstr(File poFile) throws IOException {
+	public static LinkedList<List<String>> getMsgstr(File poFile)
+			throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(poFile));
-		List<List<String>> msgstrList = new LinkedList<List<String>>();
+		LinkedList<List<String>> msgstrList = new LinkedList<List<String>>();
 		List<String> msgstr;
 
 		String newline = br.readLine();
-		while (true) {
-			if (!br.ready())
-				break;
+		while (br.ready()) {
 			if (newline.startsWith("msgstr")) {
 				msgstr = new ArrayList<String>();
 				msgstr.add(newline);
 				for (;;) {
 					newline = br.readLine();
+					if (newline.startsWith("msgid_plural")) {
+						String temp = msgstr.get(0).replace("msgstr",
+								"msgstr[0]");
+						msgstr.remove(0);
+						msgstr.add(0, temp);
+						newline = newline.replace("msgid_plural", "msgstr[1]");
+						newline.trim();
+					}
 					if (newline.startsWith("msgstr")
-							|| newline.startsWith("msgid")
-							|| newline == null)
+							|| newline.startsWith("msgid") || newline == null)
 						break;
 					msgstr.add(newline);
 				}
 				msgstrList.add(msgstr);
 			} else {
 				newline = br.readLine();
+
 			}
 		}
 
+		msgstr = new ArrayList<String>();
+		msgstr.add(newline);
+		msgstrList.add(msgstr);
+
+
+
+		System.out.println(msgstrList.get(msgstrList.size()-1));
 		br.close();
 		return msgstrList;
 	}
+
+	public static void mergePOFile(File orgPOFile, File msgstrPOFile,
+			File desFile) throws IOException {
+		BufferedReader orgFile = new BufferedReader(new FileReader(orgPOFile));
+		PrintWriter pw = new PrintWriter(desFile);
+
+		// Build msgstr list
+		Queue<List<String>> msgstrList = getMsgstr(msgstrPOFile);
+
+		// Begin merge data
+
+		while (orgFile.ready()) {
+			String newline = orgFile.readLine();
+			if (newline.startsWith("msgstr")) {
+				List<String> msgstr = msgstrList.poll();
+				for(String line : msgstr){
+					pw.println(line);
+				}
+				pw.flush();
+
+			} else {
+				pw.println(newline);
+				pw.flush();
+			}
+		}
+
+		orgFile.close();
+		pw.close();
+
+		System.out.println("Done!");
+	}
+
+	public static void writeListToFile(List<List<String>> lst, File outFile)
+			throws FileNotFoundException {
+		PrintWriter pw = new PrintWriter(outFile);
+
+		for (List<String> memLst : lst) {
+			for (String line : memLst) {
+				pw.println(line);
+				pw.flush();
+			}
+		}
+		pw.close();
+	}
+
+	public static void repairMsgstrFile(File msgidPOFile, File msgstrPOFile)
+			throws IOException {
+		BufferedReader msgidFile = new BufferedReader(new FileReader(
+				msgidPOFile));
+		BufferedReader msgstrFile = new BufferedReader(new FileReader(
+				msgstrPOFile));
+
+		PrintWriter repairedFile = new PrintWriter(new File("repaied" + msgstrPOFile.getName()));
+
+		while (msgidFile.ready() && msgstrFile.ready()) {
+			String msgid = msgidFile.readLine();
+			String msgstr = msgstrFile.readLine();
+
+			if (msgid.startsWith("msgid") && !msgid.startsWith("msgid_plural")
+					&& !msgstr.startsWith("msgstr")) {
+				if(msgstr.endsWith("msgstr")){
+					msgstr = msgstr.substring(0, msgstr.indexOf("msgstr"));
+					msgstr = msgstr.trim();
+				}
+				msgstr = "msgstr ".concat(msgstr);
+			}
+			repairedFile.println(msgstr);
+		}
+
+		msgidFile.close();
+		msgstrFile.close();
+		repairedFile.close();
+		System.out.println("done");
+	}
+
+
 }
